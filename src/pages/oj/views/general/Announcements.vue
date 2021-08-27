f3417429dc802041a7794ad9d8db32ed203b00de
Vũ Ngọc Quốc Khánh <khanhvu212@gmail.com>
2021-08-27 13:53:47 +0700

    Add shadow

--- src/pages/oj/views/general/Announcements.vue
+++ src/pages/oj/views/general/Announcements.vue
@@ -152,6 +152,7 @@
   .announcements-container {
     margin-top: -10px;
     margin-bottom: 10px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     li {
       padding-top: 15px;
       list-style: none;
